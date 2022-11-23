ec350c321eac562d31b621b8bf4b3c155be89fe3
YYinBigBang <yy32438084@gmail.com>
2022-11-23 22:30:49 +0800

    version 1.1 1.The NEXT button which could show the random words without repetition. 2.Add the button in navigation bar to reload words database.

--- vocabulary.py
+++ vocabulary.py
@@ -1,35 +1,67 @@
-"""version 1.0"""
+"""version 1.1"""
 import tkinter as tk
+from tkinter import messagebox
 import csv
 import random
 
-def build_db():
-    global vocabulary
-    vocabulary = {}
-    with open('word_db.csv', newline='', encoding="utf-8") as csvfile:
-        rows = csv.DictReader(csvfile)
-        index = 0
-        for row in rows:
-            vocabulary[index] = {'ENG': row['EN_word'], 'CHI': row['CH_word']}
-            index += 1
-        print(f"{index} words have been loaded!!")
+class vocabulary:
+    def __init__(self):
+        self.words = self.csv_load()
+        self.pool = self.update()
+
+    def csv_load(self):
+        """Load vocavulary from csv file"""
+        self.words = {}
+        with open('word_db.csv', newline='', encoding="utf-8") as csvfile:
+            rows = csv.DictReader(csvfile)
+            index = 0
+            for row in rows:
+                self.words[index] = {'ENG': row['EN_word'], 'CHI': row['CH_word']}
+                index += 1
+            print(f"{index} words have been loaded!")
+        return self.words
+
+    def update(self):
+        """Update words pool"""
+        self.pool = [index for index in range(0, len(self.words))]
+        return random.sample(self.pool, len(self.words))
+
+    def load(self):
+        return self.update(), self.csv_load()
+
+
 
 def main():
+    voc = vocabulary()
     window = tk.Tk()# build frame
     window.iconbitmap('alphabet.ico')
     window.title('Vocabulary')# set title
     menubar = tk.Menu(window)
     window.config(menu=menubar)
-    menubar.add_command(label='背單字', command=frame1(window))
-    menubar.add_command(label='單字測驗', command=frame2(window))
-    frame1(window)
+    menubar.add_command(label='背單字', command=frame1)
+    menubar.add_command(label='單字測驗', command=frame2)
+    menubar.add_command(label='更新單字', command=voc.load)
+    frame1(window, voc)
 
     window.mainloop()
 
-def frame1(window):
+
+def take_word(voc):
+    if voc.pool:
+        num = voc.pool[0]
+        voc.pool.pop(0)
+    else:
+        messagebox.showinfo('Message', f"恭喜看完一遍，共{len(voc.words)}個單字")
+        voc.update()
+        num = vocabulary().pool[0]
+        voc.pool.pop(0)
+    return voc.words[num]
+
+
+def frame1(window, voc):
+
     def show_word():
-        num = random.randint(0, len(vocabulary)-1)
-        words = vocabulary[num]
+        words = take_word(voc)
         en_word.set(words['ENG'])
         ch_word.set(words['CHI'])
 
@@ -45,11 +77,11 @@ def frame1(window):
     div2.grid(column=1, row=0)
     next_btn.grid(column=0, row=1, columnspan=2)
 
+
 def frame2(window):
     pass
 
 
 
 if __name__ == "__main__":
-    build_db()
     main()
