e3cdce35d03c572b1d22836dcfd6d38e0aed8964
YYinBigBang <yy32438084@gmail.com>
2022-12-21 21:26:06 +0800

    Add exam function and switch frame

--- vocabulary.py
+++ vocabulary.py
@@ -1,12 +1,14 @@
-"""version 1.1"""
+"""version 2.0"""
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, Radiobutton
 import csv
 import random
+
 
 class vocabulary:
     def __init__(self):
         self.words = self.csv_load()
+        self.words_index = [index for index in range(0, len(self.words))]
         self.pool = self.update()
 
     def csv_load(self):
@@ -23,65 +25,177 @@ class vocabulary:
 
     def update(self):
         """Update words pool"""
-        self.pool = [index for index in range(0, len(self.words))]
-        return random.sample(self.pool, len(self.words))
+        return random.sample(self.words_index, len(self.words))
 
-    def load(self):
-        return self.update(), self.csv_load()
+    def load_pool(self):
+        self.pool = self.update()
+        return self.pool
 
+    def take_word(self):
+        if self.pool:
+            num = self.pool[0]
+            self.pool.pop(0)
+        else:
+            messagebox.showinfo('Message', f"恭喜看完一遍，共{len(self.words)}個單字")
+            self.load_pool()
+            num = self.pool[0]
+            self.pool.pop(0)
+        return self.words[num]
 
+    def take_ques(self):
+        # Choose words number
+        if self.pool:
+            ques_num = self.pool[0]
+            self.pool.pop(0)
+        else:
+            messagebox.showinfo('Message', f"試題結束!!重整題庫")
+            self.load_pool()
+            ques_num = self.pool[0]
+            self.pool.pop(0)
 
-def main():
-    voc = vocabulary()
-    window = tk.Tk()# build frame
-    window.iconbitmap('alphabet.ico')
-    window.title('Vocabulary')# set title
-    menubar = tk.Menu(window)
-    window.config(menu=menubar)
-    menubar.add_command(label='背單字', command=frame1)
-    menubar.add_command(label='單字測驗', command=frame2)
-    menubar.add_command(label='更新單字', command=voc.load)
-    frame1(window, voc)
+        # Choose answer option
+        items = random.sample(self.words_index, 4)
+        for repeat in items:
+            if ques_num == repeat:
+                return ques_num, items
 
-    window.mainloop()
-
-
-def take_word(voc):
-    if voc.pool:
-        num = voc.pool[0]
-        voc.pool.pop(0)
-    else:
-        messagebox.showinfo('Message', f"恭喜看完一遍，共{len(voc.words)}個單字")
-        voc.update()
-        num = vocabulary().pool[0]
-        voc.pool.pop(0)
-    return voc.words[num]
-
-
-def frame1(window, voc):
-
-    def show_word():
-        words = take_word(voc)
-        en_word.set(words['ENG'])
-        ch_word.set(words['CHI'])
-
-    en_word = tk.StringVar()
-    ch_word = tk.StringVar()
-    en_word.set("English")
-    ch_word.set("Chinese")
-    div1 = tk.Label(window, height=4, width=20, textvariable=en_word, font=("Helvetica", 15))
-    div2 = tk.Label(window, height=4, width=20, textvariable=ch_word, font=("Helvetica", 15))
-    next_btn = tk.Button(window, text='Next word', height=2, width=15, command=show_word, font=("Helvetica", 15))
-
-    div1.grid(column=0, row=0)
-    div2.grid(column=1, row=0)
-    next_btn.grid(column=0, row=1, columnspan=2)
+        items[random.randint(0, 3)] = ques_num
+        return ques_num, items
 
 
-def frame2(window):
-    pass
+
+class main_window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.iconbitmap('alphabet.ico')
+        self.title('Vocabulary')
+        self.font_type = ("Helvetica", 15)
+
+        menubar = tk.Menu(self)
+        option1 = tk.Menu(menubar, tearoff=0)
+        option1.add_command(label="單字卡", command=self.show_frame1)
+        option1.add_command(label="單字測驗", command=self.show_frame2)
+        option1.add_separator()
+        option1.add_command(label="回選單", command=self.show_menuFrame)
+        menubar.add_cascade(label="模式", menu=option1)
+        self.config(menu=menubar)
+
+        # Create the frames
+        self.menuFrame = tk.Frame(self)
+        self.frame1 = tk.Frame(self)
+        self.frame2 = tk.Frame(self)
+
+        # Create the frame elements
+        self.menuFrame_element()
+        self.frame1_element()
+        self.frame2_element()
+
+        # Show the first frame by default
+        self.show_menuFrame()
+
+
+    def menuFrame_element(self):
+        pick = tk.Label(self.menuFrame, height=2, width=20, text="選擇模式", font=self.font_type)
+        mode1_btn = tk.Button(self.menuFrame, text='單字卡', height=2, width=15, command=self.show_frame1, font=self.font_type)
+        mode2_btn = tk.Button(self.menuFrame, text='單字測驗', height=2, width=15, command=self.show_frame2, font=self.font_type)
+        pick.grid(column=0, row=0)
+        mode1_btn.grid(column=0, row=1)
+        mode2_btn.grid(column=0, row=2)
+
+
+    def frame1_element(self):
+        voc = vocabulary()
+        def show_word():
+            words = voc.take_word()
+            en_word.set(words['ENG'])
+            ch_word.set(words['CHI'])
+
+        en_word = tk.StringVar()
+        ch_word = tk.StringVar()
+        en_word.set("English")
+        ch_word.set("Chinese")
+        div1 = tk.Label(self.frame1, height=4, width=20, textvariable=en_word, font=self.font_type)
+        div2 = tk.Label(self.frame1, height=4, width=20, textvariable=ch_word, font=self.font_type)
+        next_btn = tk.Button(self.frame1, text='Next word', height=2, width=15, command=show_word, font=self.font_type)
+
+        div1.grid(column=0, row=0)
+        div2.grid(column=1, row=0)
+        next_btn.grid(column=0, row=1, columnspan=2)
+
+
+    def frame2_element(self):
+        voc = vocabulary()
+        def words_set(ques_num, items):
+            global radio_item
+            # print("______出題______")
+            # print("題目編號:", ques_num)
+            # print("選項:", items)
+            question.set(ques_num)
+            radio_item = []
+            for item in items:
+                radio_item.append(item)
+            en_word.set(voc.words[ques_num]['ENG'])
+            ch_word1.set(voc.words[radio_item[0]]['CHI'])
+            ch_word2.set(voc.words[radio_item[1]]['CHI'])
+            ch_word3.set(voc.words[radio_item[2]]['CHI'])
+            ch_word4.set(voc.words[radio_item[3]]['CHI'])
+
+        def submit(ques_num, ans):
+            # print("===送出答案===")
+            # print("題目ques", ques_num)
+            # print("ans選擇答案為", radio_item[ans])
+            # print(voc.words[radio_item[ans]]['CHI'])
+            if radio_item[ans] == ques_num:
+                messagebox.showinfo('The result is ~', f"恭喜答對")
+            else:
+                messagebox.showinfo('The result is ~', f"答錯囉~答案為:{voc.words[ques_num]['CHI']}")
+            ques_num, items = voc.take_ques()
+            words_set(ques_num, items)
+
+        ques_num, items = voc.take_ques()
+
+        en_word = tk.StringVar()
+        ch_word1 = tk.StringVar()
+        ch_word2 = tk.StringVar()
+        ch_word3 = tk.StringVar()
+        ch_word4 = tk.StringVar()
+        question = tk.IntVar()
+        ans = tk.IntVar()
+        words_set(ques_num, items)
+
+        div = tk.Label(self.frame2, height=4, width=20, textvariable=en_word, font=self.font_type)
+        option1 = Radiobutton(self.frame2, textvariable=ch_word1, value=0, variable=ans, font=self.font_type)
+        option2 = Radiobutton(self.frame2, textvariable=ch_word2, value=1, variable=ans, font=self.font_type)
+        option3 = Radiobutton(self.frame2, textvariable=ch_word3, value=2, variable=ans, font=self.font_type)
+        option4 = Radiobutton(self.frame2, textvariable=ch_word4, value=3, variable=ans, font=self.font_type)
+        option1.grid(column=1, row=0, sticky='w')
+        option1.select()
+        option2.grid(column=1, row=1, sticky='w')
+        option3.grid(column=1, row=2, sticky='w')
+        option4.grid(column=1, row=3, sticky='w')
+        check_btn = tk.Button(self.frame2, text='提交', height=2, width=15, font=self.font_type,
+                              command=lambda: submit(question.get(), ans.get()))
+        div.grid(column=0, row=0, rowspan=4)
+        check_btn.grid(column=0, row=4, columnspan=2)
+
+
+    def show_menuFrame(self):
+        self.frame1.pack_forget()
+        self.frame2.pack_forget()
+        self.menuFrame.pack()
+
+    def show_frame1(self):
+        self.menuFrame.pack_forget()
+        self.frame2.pack_forget()
+        self.frame1.pack()
+
+    def show_frame2(self):
+        self.menuFrame.pack_forget()
+        self.frame1.pack_forget()
+        self.frame2.pack()
 
 
 
 if __name__ == "__main__":
-    main()
+    app = main_window()
+    app.mainloop()
